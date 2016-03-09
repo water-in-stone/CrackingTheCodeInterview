58d04db759a1c94137b50db3b3da437ea87207f2
Young <523464427@qq.com>
2016-03-09 15:42:51 +0800

    add my solution to github

---
+++ src/Exam1/Test.java
@@ -0,0 +1,11 @@
+package Exam1;
+
+/**
+ * Created by Young on 2016/3/8.
+ * wang645788@gmail.com
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1 << 3);
+    }
+}

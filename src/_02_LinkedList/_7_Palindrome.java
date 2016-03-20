a70ded1b067c4baa589edfe1df99d62622a51420
Young <523464427@qq.com>
2016-03-20 14:30:38 +0800

    optimize ListNode_3

--- src/_02_LinkedList/_7_Palindrome.java
+++ src/_02_LinkedList/_7_Palindrome.java
@@ -1,3 +1,4 @@
+package _02_LinkedList;
 import java.util.*;
 
 /*
@@ -14,7 +15,8 @@ public class ListNode {
 // 对数组进行对半比对，若不相同，则直接返回false
 public class Palindrome {
     public boolean isPalindrome(ListNode pHead) {
-        int[] arrValue = new int[400];
+    	//其实这里还是用列表比较好，这样不管多长都可以判断
+        /*int[] arrValue = new int[400];
         int index = 0; //表示要取值放入值数组的初始下标
         while (pHead.next != null) {
         	arrValue[index++] = pHead.val;
@@ -26,6 +28,24 @@ public class Palindrome {
         		return false;
         	}
         }
+        return true;*/
+        if (pHead == null) {
+        	return false;
+        }
+        ArrayList<Integer> listValue = new ArrayList<>();
+        int length;
+        while(pHead.next != null){
+        	listValue.add(pHead.val);
+        	pHead = pHead.next;
+        }
+        listValue.add(pHead.val);
+        length = listValue.size();
+        for (int i = 0; i < length / 2; i++) {
+        	if(!listValue.get(i).equals(listValue.get(length - 1 - i))){
+        		return false;
+        	}
+        }
         return true;
     }
 }
+
